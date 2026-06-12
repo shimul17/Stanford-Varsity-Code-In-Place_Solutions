e64f3a66985e58d576094dcca9eef29be44c565b
Haran Kanta Dey <harandey12@gmail.com>
2026-06-12 17:48:39 +0600

    multiply_2_nums.py

---
+++ Week_3: Intro_to_Python/multiply_2-nums.py
@@ -0,0 +1,9 @@
+def main():
+    print("This program multiplies two numbers.")
+    num1 = int(input("Enter first number: " ))
+    num2 = int(input("Enter second number: "))
+    product = num1 * num2
+    print(product)
+
+if __name__ == '__main__':
+    main()
