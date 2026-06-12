09a542b80f1d44bfe3ee742d22c6bd021a441095
Haran Kanta Dey <harandey12@gmail.com>
2026-06-12 18:20:19 +0600

    liftoff.py

---
+++ Week_4: Control Flow/liftoff.py
@@ -0,0 +1,13 @@
+"""
+Program: Liftoff
+--------------------
+Countdown from 10 to 1 and then print Liftoff!
+"""
+
+def main():
+    for i in range(10, 0, -1):
+        print(i)
+    print("Liftoff!")
+
+if __name__ == '__main__':
+    main()
