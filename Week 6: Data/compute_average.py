bfe9586c52dec221c91a4bb0dc8c9cf35cd9e596
Haran Kanta Dey <harandey12@gmail.com>
2026-06-12 19:40:35 +0600

    Update compute_average.py

--- Week 6: Data/compute_average.py
+++ Week 6: Data/compute_average.py
@@ -1,1 +1,26 @@
+def main():
+    number_list = load_numbers_from_file("numbers.txt")
+    if len(number_list) > 0:
+        average = sum(number_list) / len(number_list)
+        print("Average:", average)
+    else:
+        print("Average: 0.0")
+
+def load_numbers_from_file(filepath):
+    """
+    Loads numbers from a file into a list and returns it.
+    We assume the file to have one number per line.
+    Returns a list of numbers. You should not modify this
+    function.
+    """
+    numbers = []
+    with open(filepath, 'r') as file_reader:
+        for line in file_reader.readlines():
+            cleaned_line = line.strip()
+            if cleaned_line != '':
+                numbers.append(float(cleaned_line))    
+    return numbers
+
+if __name__ == '__main__':
+    main()
 
