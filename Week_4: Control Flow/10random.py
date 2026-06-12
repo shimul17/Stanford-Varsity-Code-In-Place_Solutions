568d9525adca4981c793a91c74e325fa5bc2785f
Haran Kanta Dey <harandey12@gmail.com>
2026-06-12 18:18:37 +0600

    10random.py

---
+++ Week_4: Control Flow/10random.py
@@ -0,0 +1,14 @@
+import random
+
+N_NUMBERS = 10
+MIN_VALUE = 1
+MAX_VALUE = 100
+
+def main():
+   
+    for value in range(10):
+        value = random.randint(1, 100)
+        print(value)
+      
+if __name__ == '__main__':
+    main()
